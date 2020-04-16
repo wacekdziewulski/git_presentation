cb3ac7e054f0484e8b0464a8b136a691c3521351
Waclaw <wacekdziewulski@gmail.com>
2020-04-16 20:32:05 +0200

    prepare.sh script will now automatically copy the SSH public key before building the docker image

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo -e "\033[1;32m*) Preparing your public ssh key to become available in authorized_keys in the docker container... \033[0m"
+cp ~/.ssh/id_rsa.pub authorized_keys
+
 echo -e "\033[1;32m*) Building docker container... \033[0m"
 docker build -t gpr:latest .
 
